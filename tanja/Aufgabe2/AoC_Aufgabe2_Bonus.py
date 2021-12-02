171e0385ee67761cd4a56e4dbb2a74499a456449
randomspawnX <tanja.maier.t@hotmail.de>
2021-12-02 14:23:01 +0100

    Aufgabe 2 für 2020 und 2021 done

---
+++ tanja/Aufgabe2/AoC_Aufgabe2_Bonus.py
@@ -0,0 +1,20 @@
+import sys
+import re
+
+
+def main():
+    horizontal = 0
+    depth = 0
+    aim = 0
+    with open('input.txt') as file:
+        for line in file:
+            movement = list(filter(None, re.split('\s', line)))
+            if(movement[0] == 'forward'):
+                horizontal += int(movement[1])
+                depth += aim*int(movement[1])
+            if (movement[0] == 'up'): aim -= int(movement[1])
+            if (movement[0] == 'down'): aim += int(movement[1])
+    print(horizontal*depth)
+
+if __name__ == '__main__':
+     sys.exit(main())
