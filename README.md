7c43aaceecff6f1ef66ab6476d89d5477f1fd20d
z0024ksx <andreas.mirring@siemens.com>
2021-12-03 12:46:25 +0100

    part-one-andüü

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # adventofcode
 
 bitte jeder in seinen ordner rein packen :)
+
+Everybody, please use your own folder :)
---
+++ andüü/03-01.py
@@ -0,0 +1,26 @@
+if __name__ == '__main__':
+    # init
+    count_ones = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    count_zeroes = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    gamma_rate_str = ''
+    epsilon_rate_str = ''
+
+    with open('03-input.txt', 'r', encoding='utf-8') as f:
+        text_input = f.readlines()
+    for row in text_input:
+        row = row.replace('\n', '')
+        for idx_row, char in enumerate(row):
+            if char == '1':
+                count_ones[idx_row] += 1
+            else:
+                count_zeroes[idx_row] += 1
+
+    for i in range(12):     # check gamma rate
+        if count_ones[i]>count_zeroes[i]:
+            gamma_rate_str+='1'
+            epsilon_rate_str+='0'
+        else:
+            gamma_rate_str+='0'
+            epsilon_rate_str+='1'
+    result = int(gamma_rate_str,2) * int(epsilon_rate_str,2)
+    print(result)
