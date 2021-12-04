102a5efb87c7e5cc1d40a1a8319e0e8464c6691d
Andi <non@none.com>
2021-12-04 15:03:08 +0100

    andüü tag 4

--- andüü/03-02.py
+++ andüü/03-02.py
@@ -20,11 +20,26 @@ if __name__ == '__main__':
     # check oxygen
     # loop through bits
     for curr_bit in range(12):
-        max_value_oxygen = '1' if count_ones[curr_bit] >= count_zeroes[curr_bit] else '0'
-        min_value_co2 = '0' if count_zeroes[curr_bit] <= count_ones[curr_bit] else '1'
         if len(oxygen) > 1:
+            count_zeroes_oxy = 0
+            count_ones_oxy = 0
+            for row in oxygen:
+                if row[curr_bit] == '1':
+                    count_ones_oxy += 1
+                else:
+                    count_zeroes_oxy += 1
+            max_value_oxygen = '1' if count_ones_oxy >= count_zeroes_oxy else '0'
             oxygen = [elem for elem in oxygen if elem[curr_bit] == max_value_oxygen]
+
         if len(co2) > 1:
+            count_zeroes_co2 = 0
+            count_ones_co2 = 0
+            for row in co2:
+                if row[curr_bit] == '1':
+                    count_ones_co2 += 1
+                else:
+                    count_zeroes_co2 += 1
+            min_value_co2 = '0' if count_zeroes_co2 <= count_ones_co2 else '1'
             co2 = [elem for elem in co2 if elem[curr_bit] == min_value_co2]
 
     print(int(oxygen[0], 2))
---
+++ andüü/04-01.py
@@ -0,0 +1,91 @@
+import sys
+
+
+def read_content():
+    with open('04-input.txt', 'r', encoding='utf-8') as f:
+        text_input = f.readlines()
+
+    text_input.append('\n')
+    input_grid = {}
+    grid_id = 0
+    new_grid = {}
+    for idx, content in enumerate(text_input):
+        content = content.replace('\n', '')
+        if idx == 0:
+            input_numbers = [int(x) for x in content.split(',')]
+            continue
+
+        if not (content):  # if empty --> separator between boards, init new grid
+            if new_grid:  # grid existing from reading
+                input_grid.update({grid_id: new_grid})
+                grid_id += 1
+            new_grid = dict()
+            y_row_id = 0
+            continue
+
+        if (content):  # input numbers for grids
+            x_row_list = [int(x) for x in content.strip().replace('  ', ' ').split(' ')]
+            x_dict = {}
+            for idx, elem in enumerate(x_row_list):
+                x_dict.update({idx: elem})
+            new_grid.update({y_row_id: x_dict})
+            y_row_id += 1
+    return input_numbers, input_grid
+
+
+def get_winning_score(input_numbers: list, input_grid: list):
+    num_boards = len(input_grid)
+    len_x = 5
+    len_y = 5
+    # draw numbers
+
+    for draw in input_numbers:
+        for board_id in range(num_boards):
+            for x in range(len_x):
+                for y in range(len_y):
+                    if input_grid[board_id][x][y] == draw:
+                        input_grid[board_id][x][y] = -1
+                        max_boardsum = check_winner_boards(input_grid)
+                        if max_boardsum != -1:
+                            return max_boardsum*draw
+    return -1
+
+
+def check_winner_boards(input_grid: dict):
+    '''check for bingo, return winner score of best board if successful'''
+    max_board_score = -1
+    for board_id in input_grid:
+        board = input_grid[board_id]
+        # check x winners
+        for row in range(5):
+            if board[0][row] == board[1][row] == board[2][row] == board[3][row] == board[4][row] == -1:
+                board_sum = get_board_score(board)
+                if board_sum > max_board_score:
+                    max_board_score = board_sum
+        # check y winners
+        for row in range(5):
+            if board[row][0] == board[row][1] == board[row][2] == board[row][3] == board[row][4] == -1:
+                board_sum = get_board_score(board)
+                if board_sum > max_board_score:
+                    max_board_score = board_sum
+    return max_board_score
+
+
+def get_board_score(board):
+    print(board)
+    board_sum = 0
+    for i in range(5):
+        for j in range(5):
+            if board[i][j] != -1:
+                board_sum += board[i][j]
+    return board_sum
+
+
+def main():
+    input_numbers, input_grid = read_content()
+    score = get_winning_score(input_numbers, input_grid)
+    print(score)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
