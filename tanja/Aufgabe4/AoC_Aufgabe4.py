212d7b333b75f0c1ef1ec64703fdf099bd64e8c1
randomspawnX <tanja.maier.t@hotmail.de>
2021-12-05 02:36:58 +0100

    task 4 sucks ass

--- tanja/Aufgabe4/AoC_Aufgabe4.py
+++ tanja/Aufgabe4/AoC_Aufgabe4.py
@@ -37,42 +37,35 @@ def main():
                 bingoBoards[i] = [(t[0], True) if t[0] == winningNumber else (t[0], t[1]) for t in bingoBoards[i]]
                 counterTrueRows = 0
                 counterTrueColumns = 0
-                counter = 0
 
-                # winningrow
+
+                counter = 0
                 for j in range(len(bingoBoards[i])):
                     if counter < bingoBoardX:
                         counter += 1
-                        if bingoBoards[i][j][1] == True:
-                            counterTrueRows += 1
-                        if (counterTrueRows == bingoBoardX):
-                            print("woheeRow")
-                            print(bingoBoards[i])
-                            print(j)
-                            BINGO = True
-                            break;
-                    else:
+                    #rownwin
+                    if bingoBoards[i][j][1] == True:
+                        counterTrueRows += 1
+                    if (counterTrueRows == bingoBoardX):
+                        print("woheeRow")
+                        print(bingoBoards[i])
+                        print(j)
+                        BINGO = True
+                        break
+
+                    #columnwin
+                    if bingoBoards[i][j % bingoBoardX * bingoBoardX + int(j / 5)][1] == True:
+                        counterTrueColumns += 1
+                    if (counterTrueColumns == bingoBoardX):
+                        print("woheeColumn")
+                        print(bingoBoards[i])
+                        BINGO = True
+                        break
+
+                    if counter >= bingoBoardX:
                         counter = 0
                         counterTrueRows = 0
-
-                # winningcolumn
-                if BINGO == False:
-                    for j in range(bingoBoardX):
-                        if(BINGO == True):
-                            break
-                        for k in range(bingoBoardX):
-                            if counter < bingoBoardX:
-                                counter += 1
-                                if bingoBoards[i][j + bingoBoardX * k][1] == True:
-                                    counterTrueColumns += 1
-                                if (counterTrueColumns == bingoBoardX):
-                                    print("woheeColumn")
-                                    print(bingoBoards[i])
-                                    Bingo = True
-                                    break
-                            else:
-                                counter = 0
-                                counterTrueColumns = 0
+                        counterTrueColumns = 0
 
                 if BINGO == True:
                     value = sum(int(t[0]) for t in bingoBoards[i] if t[1] != True)
@@ -80,10 +73,9 @@ def main():
                     print(winningNumber)
                     print(i)
                     print(len(bingoBoards))
-                    print(value*int(winningNumber))
+                    print(value * int(winningNumber))
+                    print(bingoBoards[45])
                     return
-
-
 
 
 if __name__ == '__main__':
