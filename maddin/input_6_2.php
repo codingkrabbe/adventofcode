e2a312e22c01c06c1312daebc90ab14d93fed263
0xd3adc0de <36731643+0xd3adc0de@users.noreply.github.com>
2021-12-06 16:50:28 +0100

    Day 6 cleaner code

--- maddin/input_6_2.php
+++ maddin/input_6_2.php
@@ -1,22 +1,22 @@
 <?php
 $input=file("input_6_1.txt");
 $data=explode(",",trim($input[0]));
-$fishes=[0,0,0,0,0,0,0,0,0];
-for ($i=0;$i<count($data);$i++) {
-$fishes[$data[$i]]+=1;
-}
-for ($i=0;$i<256;$i++){
+$fishes=array_fill(0,9,0);
+$result=0;
 
-$fishes_born=$fishes[0];
-$fishes[0]=$fishes[1];
-$fishes[1]=$fishes[2];
-$fishes[2]=$fishes[3];
-$fishes[3]=$fishes[4];
-$fishes[4]=$fishes[5];
-$fishes[5]=$fishes[6];
-$fishes[6]=$fishes[7]+$fishes_born;
-$fishes[7]=$fishes[8];
-$fishes[8]=$fishes_born;
+for ($i=0;$i<count($data);$i++) {
+        $fishes[$data[$i]]+=1;
 }
-echo "Result: ".($fishes[0]+$fishes[1]+$fishes[2]+$fishes[3]+$fishes[4]+$fishes[5]+$fishes[6]+$fishes[7]+$fishes[8]);
+
+for ($i=0;$i<256;$i++){
+        $fishes_born=$fishes[0];
+        array_splice($fishes,0,1);
+        $fishes[6]+=$fishes_born;
+        $fishes[8]=$fishes_born;
+}
+
+for ($i=0;$i<count($fishes);$i++) {
+        $result+=$fishes[$i];
+}
+echo "Result: $result";
 ?>
