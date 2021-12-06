34e96b6b28903d69da2cd7f0d727fd4866650748
z003pfuj <jonathan.beller@siemens.com>
2021-12-06 09:40:34 +0100

    added stub files

--- jonathan/Aufgabe4/2.py
+++ jonathan/Aufgabe4/2.py
@@ -10,6 +10,7 @@ class BingoField:
 class BingoBoard:
     def __init__(self, numbers: list):
         self.board = [[], [], [], [], []]
+        self.already_won = False
 
         for row_idx, _ in enumerate(numbers):
             for number in numbers[row_idx]:
@@ -29,15 +30,14 @@ class BingoBoard:
                 if not bingo_field.marked:
                     all_marked = False
             if all_marked:
-                return True
+                self.already_won = True
         for col_idx in range(len(self.board[0])):
             all_marked = True
             for row_idx in range(len(self.board)):
                 if not self.board[row_idx][col_idx].marked:
                     all_marked = False
             if all_marked:
-                return True
-        return False
+                self.already_won = True
 
     def calculate_score(self, last_marked_number):
         score = 0
@@ -65,14 +65,22 @@ def main():
     lines = open('input.txt', 'r').readlines()
     drawn_numbers = lines[0].split(',')
     bingo_boards = read_bingo_boards(lines[2:])
-    game_over = False
+    board_to_check = None
+    score_printed = False
     for drawn_number in drawn_numbers:
-        for bingo_board in bingo_boards:
-            if not game_over:
+        if not score_printed:
+            for bingo_board in bingo_boards:
                 bingo_board.mark_number(int(drawn_number))
-                game_over = bingo_board.check_for_win()
-                if game_over:
-                    print(bingo_board.calculate_score(int(drawn_number)))
+            boards_not_yet_won = []
+            for bingo_board in bingo_boards:
+                if board_to_check is None and not bingo_board.already_won:
+                    boards_not_yet_won.append(bingo_board)
+            if board_to_check is None and len(boards_not_yet_won) == 1:
+                board_to_check = boards_not_yet_won[0]
+            if board_to_check is not None:
+                if board_to_check.already_won:
+                    print(board_to_check.calculate_score(int(drawn_number)))
+                    score_printed = True
 
 
 if __name__ == '__main__':
