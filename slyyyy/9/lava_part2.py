2bdedfb046c1044f7af63b741a2eaa5230202580
TravSalesman <arcus.cirrus@outlook.com>
2021-12-09 13:56:08 +0100

    Day 9

---
+++ slyyyy/9/lava_part2.py
@@ -0,0 +1,52 @@
+import os, sys, math
+
+neighbours = [(0,-1), (1,0), (0,1), (-1,0)]
+
+def calc_basin_recursive(coord, mapp, covered_list):
+	summ = 0
+	for n in neighbours:
+		if(coord[0]+n[0] >= 0 and coord[0]+n[0] < len(mapp[coord[1]]) and
+		   coord[1]+n[1] >= 0 and coord[1]+n[1] < len(mapp)):
+			if(mapp[coord[1]][coord[0]] < mapp[coord[1]+n[1]][coord[0]+n[0]] and
+			   mapp[coord[1]+n[1]][coord[0]+n[0]] < 9):
+				new_coord = (coord[0]+n[0], coord[1]+n[1])
+				if(not new_coord in covered_list):
+					covered_list.append(new_coord)
+					summ += calc_basin_recursive(new_coord, mapp, covered_list) + 1
+	return summ
+
+
+def main():
+	input_values = []
+	lows = []
+	with open("input.txt", 'r') as file:
+		for line in file.readlines():
+			input_values.append([int(x) for x in line if(x !='\n')])
+	
+	for i in range(len(input_values)):
+		for j in range(len(input_values[i])):
+			summ = 0
+			count = 0
+			for n in neighbours:
+				if(j+n[0] >= 0 and j+n[0] < len(input_values[i]) and
+				   i+n[1] >= 0 and i+n[1] < len(input_values)):
+					if(input_values[i][j] < input_values[i+n[1]][j+n[0]]):
+						summ += 1
+					count += 1
+			if(summ == count):
+				lows.append((j,i))
+
+	basin_list = []
+	for p in lows:
+		covered_list = [p] # add already inspected coords to prevent adding duplicates to basin
+		basin_list.append(1 + calc_basin_recursive(p, input_values, covered_list))
+
+	basin_list.sort()
+	result = basin_list[len(basin_list)-1] * basin_list[len(basin_list)-2] * basin_list[len(basin_list)-3]
+
+	print("Sum of Risk Levels: " + str(result))
+
+
+if __name__ == "__main__":
+	sys.exit(main())
+
