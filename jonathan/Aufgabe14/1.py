191cf752aa5e6350fb1b00f8055c71c51c53e915
z003pfuj <jonathan.beller@siemens.com>
2021-12-14 21:14:24 +0100

    jonny 14

--- jonathan/Aufgabe14/1.py
+++ jonathan/Aufgabe14/1.py
@@ -1,5 +1,29 @@
+def get_most_and_least_frequencies(input: str):
+    character_occurrences = dict()
+    for char in list(input):
+        if char in character_occurrences.keys():
+            character_occurrences[char] += 1
+        else:
+            character_occurrences[char] = 1
+    return max(character_occurrences.values()), min(character_occurrences.values())
+
+
 def main():
     lines = open('input.txt', 'r').readlines()
+    template = lines[0].strip()
+    replacement_rules = dict()
+    for line in lines[2:]:
+        placeholder, inserted_char = line.strip().split(' -> ')
+        replacement_rules[placeholder] = inserted_char
+    for i in range(10):
+        chars_to_insert = []
+        for j in range(len(template) - 1):
+            chars_to_insert.append(replacement_rules[template[j:j + 2]])
+        for j in range(len(chars_to_insert)):
+            template = template[:2 * j + 1] + chars_to_insert[j] + template[2 * j + 1:]
+    max_occurrences, min_occurrences = get_most_and_least_frequencies(template)
+    print('Difference of most and least frequent character: ' + str(max_occurrences - min_occurrences))
+
 
 
 if __name__ == '__main__':
--- jonathan/Aufgabe14/2.py
+++ jonathan/Aufgabe14/2.py
@@ -1,5 +1,62 @@
+def get_most_and_least_frequencies(first_char: str, input: dict):
+    char_occurrences = {}
+    for source, frequency in input.items():
+        if source[-1] in char_occurrences.keys():
+            char_occurrences[source[-1]] += frequency
+        else:
+            char_occurrences[source[-1]] = frequency
+    char_occurrences[first_char] += 1
+    return max(char_occurrences.values()), min(char_occurrences.values())
+
+
+def get_resulting_pairs(source: str, replacement_rules: dict):
+    if source in replacement_rules.keys():
+        char = replacement_rules[source]
+        return f'{source[0]}{char}', f'{char}{source[1]}'
+    else:
+        return None, None
+
+
 def main():
     lines = open('input.txt', 'r').readlines()
+    template = lines[0].strip()
+    occurrences = {}
+    last_occurrences = {}
+    replacement_rules = {}
+    for line in lines[2:]:
+        placeholder, inserted_char = line.strip().split(' -> ')
+        replacement_rules[placeholder] = inserted_char
+    for j in range(len(template) - 1):
+        str1, str2 = get_resulting_pairs(template[j:j + 2], replacement_rules)
+        if str1 in last_occurrences.keys():
+            last_occurrences[str1] += 1
+        else:
+            last_occurrences[str1] = 1
+        if str2 in last_occurrences.keys():
+            last_occurrences[str2] += 1
+        else:
+            last_occurrences[str2] = 1
+    for i in range(39):
+        print(i)
+        for source in occurrences.keys():
+            occurrences[source] = 0
+        for source, frequency in last_occurrences.items():
+            str1, str2 = get_resulting_pairs(source, replacement_rules)
+            if str1 in occurrences:
+                occurrences[str1] += frequency
+            else:
+                occurrences[str1] = frequency
+            if str2 in occurrences:
+                occurrences[str2] += frequency
+            else:
+                occurrences[str2] = frequency
+        last_occurrences = occurrences.copy()
+        max_occurrences, min_occurrences = get_most_and_least_frequencies(template[0], occurrences)
+    expected_total_chars = len(template) * 2 ** 40 + 1 - (2 ** 40)
+    actual_total_chars = sum(occurrences.values())
+    print('expected:', expected_total_chars, ', actual:', actual_total_chars)
+    max_occurrences, min_occurrences = get_most_and_least_frequencies(template[0], occurrences)
+    print('Difference of most and least frequent character: ' + str(max_occurrences - min_occurrences))
 
 
 if __name__ == '__main__':
