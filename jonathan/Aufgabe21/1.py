7ba9bbc542b8add52b025cd30ad2cc7b53c84329
z003pfuj <jonathan.beller@siemens.com>
2021-12-22 01:56:26 +0100

    jonny 21

--- jonathan/Aufgabe21/1.py
+++ jonathan/Aufgabe21/1.py
@@ -1,5 +1,33 @@
 def main():
     lines = open('input.txt', 'r').readlines()
+    pos_1 = int(lines[0].strip().split()[-1])
+    pos_2 = int(lines[1].strip().split()[-1])
+    score_1 = 0
+    score_2 = 0
+    next_die_value = 1
+    num_die_rolls = 0
+    while score_1 < 1000 and score_2 < 1000:
+        num_fields_to_move = 0
+        for i in range(3):
+            num_fields_to_move += next_die_value
+            next_die_value += 1
+            num_die_rolls += 1
+            if next_die_value > 100:
+                next_die_value = 1
+        pos_1 = ((pos_1 + (num_fields_to_move % 10) - 1) % 10) + 1
+        score_1 += pos_1
+        if score_1 < 1000:
+            num_fields_to_move = 0
+            for i in range(3):
+                num_fields_to_move += next_die_value
+                next_die_value += 1
+                num_die_rolls += 1
+                if next_die_value > 100:
+                    next_die_value = 1
+            pos_2 = ((pos_2 + (num_fields_to_move % 10) - 1) % 10) + 1
+            score_2 += pos_2
+    losing_score = min(score_1, score_2)
+    print(f'Product of losing score and die value: {losing_score * num_die_rolls}')
 
 
 if __name__ == '__main__':
--- jonathan/Aufgabe21/2.py
+++ jonathan/Aufgabe21/2.py
@@ -1,5 +1,39 @@
+import functools
+
+FREQUENCIES = (1, 3, 6, 7, 6, 3, 1)
+VALUES = (3, 4, 5, 6, 7, 8, 9)
+
+
+@functools.lru_cache(maxsize=None)
+def perform_turn(pos_1, score_1, pos_2, score_2):
+    if score_1 >= 21:
+        return 1, 0
+    if score_2 >= 21:
+        return 0, 1
+
+    total_wins_1 = 0
+    total_wins_2 = 0
+
+    for i in range(len(FREQUENCIES)):
+        new_pos_1 = (pos_1 + VALUES[i] - 1) % 10 + 1
+        new_score_1 = score_1 + new_pos_1
+        wins_2, wins_1 = perform_turn(pos_2, score_2, new_pos_1, new_score_1)
+        total_wins_1 += wins_1 * FREQUENCIES[i]
+        total_wins_2 += wins_2 * FREQUENCIES[i]
+
+    return total_wins_1, total_wins_2
+
+
+
 def main():
     lines = open('input.txt', 'r').readlines()
+    pos_1 = int(lines[0].strip().split()[-1])
+    pos_2 = int(lines[1].strip().split()[-1])
+    score_1 = 0
+    score_2 = 0
+    universes_won_1, universes_won_2 = perform_turn(pos_1, score_1, pos_2, score_2)
+    max_universes_won = max(universes_won_1, universes_won_2)
+    print(f'Max universes won: {max_universes_won}')
 
 
 if __name__ == '__main__':
